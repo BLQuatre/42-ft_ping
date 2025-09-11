6207fdb06354131f825ec776cfb3356c05aa0595
BL4 <clem29auv@gmail.com>
2025-09-11 17:21:57 +0200

    refactor: comment out unused show_args and show_info functions for clarity

--- srcs/main.c
+++ srcs/main.c
@@ -2,31 +2,31 @@
 
 int ping_loop = 1;
 
-static void show_args(t_ping_args args) {
-	printf("Args:\n");
-	for (int i = 0; args.adresses && args.adresses[i]; i++) {
-		printf("  address[%d]: %s\n", i, args.adresses[i]);
-	}
-	printf("  verbose: %d\n", args.options & OPT_VERBOSE);
-	printf("  quiet: %d\n", args.options & OPT_QUIET);
-	printf("  interval: %ld\n", args.interval);
-	printf("  count: %ld\n", args.count);
-	printf("  tos: %d\n", args.tos);
-	printf("  timeout: %d\n", args.timeout);
-	printf("  linger: %d\n", args.linger);
-	printf("  size: %ld\n", args.size);
-	printf("  flood: %d\n", args.options & OPT_FLOOD);
-	printf("  preload: %ld\n", args.preload);
-}
+// static void show_args(t_ping_args args) {
+// 	printf("Args:\n");
+// 	for (int i = 0; args.adresses && args.adresses[i]; i++) {
+// 		printf("  address[%d]: %s\n", i, args.adresses[i]);
+// 	}
+// 	printf("  verbose: %d\n", args.options & OPT_VERBOSE);
+// 	printf("  quiet: %d\n", args.options & OPT_QUIET);
+// 	printf("  interval: %ld\n", args.interval);
+// 	printf("  count: %ld\n", args.count);
+// 	printf("  tos: %d\n", args.tos);
+// 	printf("  timeout: %d\n", args.timeout);
+// 	printf("  linger: %d\n", args.linger);
+// 	printf("  size: %ld\n", args.size);
+// 	printf("  flood: %d\n", args.options & OPT_FLOOD);
+// 	printf("  preload: %ld\n", args.preload);
+// }
 
-static void show_info(t_ping_info info) {
-	printf("Info:\n");
-	printf("  hostname: %s\n", info.hostname);
-	printf("  ip_addr: %s\n", info.ip_addr);
-	printf("  addr_con.sin_family: %d\n", info.addr_con.sin_family);
-	printf("  addr_con.sin_port: %d\n", ntohs(info.addr_con.sin_port));
-	printf("  addr_con.sin_addr.s_addr: %u\n", ntohl(info.addr_con.sin_addr.s_addr));
-}
+// static void show_info(t_ping_info info) {
+// 	printf("Info:\n");
+// 	printf("  hostname: %s\n", info.hostname);
+// 	printf("  ip_addr: %s\n", info.ip_addr);
+// 	printf("  addr_con.sin_family: %d\n", info.addr_con.sin_family);
+// 	printf("  addr_con.sin_port: %d\n", ntohs(info.addr_con.sin_port));
+// 	printf("  addr_con.sin_addr.s_addr: %u\n", ntohl(info.addr_con.sin_addr.s_addr));
+// }
 
 static void intHandler(int dummy) {
 	(void) dummy;
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
 	argv[0] = basename(argv[0]);
 
 	args = parse_args(argc, argv);
-	show_args(args);
+	// show_args(args);
 
 	sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (sockfd < 0) {
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	info = parse_ping_info(args.adresses[0], argv[0]);
-	show_info(info);
+	// show_info(info);
 
 	printf("PING %s (%s): %ld data bytes", info.hostname, info.ip_addr, args.size);
 	if (args.options & OPT_VERBOSE) {

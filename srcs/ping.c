dcac76812bfa49951f601ec0ec931d2ba914cc3f
BL4 <clem29auv@gmail.com>
2025-09-11 17:18:42 +0200

    refactor: streamline socket option settings and improve readability in send_ping function

--- srcs/ping.c
+++ srcs/ping.c
@@ -57,39 +57,16 @@ void send_ping(int ping_sockfd, t_ping_info *info, t_ping_args *args) {
 	// Set socket options at IP to TTL and value to 64
 	if (setsockopt(ping_sockfd, SOL_IP, IP_TTL, &ttl_val, sizeof(ttl_val)) != 0) {
 		printf("ft_ping: setsockopt: TTL failed\n");
-		if (args->options & OPT_VERBOSE) {
-			perror("ft_ping: setsockopt TTL");
-		}
 		return;
 	}
 
 	// Set TOS (Type of Service) if specified
 	if (args->tos >= 0) {
-		if (setsockopt(ping_sockfd, SOL_IP, IP_TOS, &args->tos, sizeof(args->tos)) != 0) {
-			if (args->options & OPT_VERBOSE) {
-				printf("ft_ping: warning: setsockopt IP_TOS failed\n");
-				perror("ft_ping: setsockopt IP_TOS");
-			}
-		} else if (args->options & OPT_VERBOSE) {
-			printf("ft_ping: TOS set to %d\n", args->tos);
-		}
+		setsockopt(ping_sockfd, SOL_IP, IP_TOS, &args->tos, sizeof(args->tos));
 	}
 
 	// Setting timeout of receive setting
-	if (setsockopt(ping_sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv_out, sizeof tv_out) != 0) {
-		if (args->options & OPT_VERBOSE) {
-			printf("ft_ping: warning: setsockopt SO_RCVTIMEO failed\n");
-			perror("ft_ping: setsockopt SO_RCVTIMEO");
-		}
-	}
-
-	if (args->options & OPT_VERBOSE) {
-		printf("ft_ping: socket configured, TTL=%d, timeout=%ds", ttl_val, (int)tv_out.tv_sec);
-		if (args->tos >= 0) {
-			printf(", TOS=%d", args->tos);
-		}
-		printf("\n");
-	}
+	setsockopt(ping_sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv_out, sizeof tv_out);
 
 	// Send ICMP packet in an infinite loop
 	while (ping_loop && (args->count == 0 || ping_count < args->count)) {
@@ -97,9 +74,6 @@ void send_ping(int ping_sockfd, t_ping_info *info, t_ping_args *args) {
 			t_timespec current_time;
 			clock_gettime(CLOCK_MONOTONIC, &current_time);
 			if (current_time.tv_sec >= timeout_end.tv_sec) {
-				if (args->options & OPT_VERBOSE) {
-					printf("ft_ping: timeout reached after %d seconds\n", args->timeout);
-				}
 				break;
 			}
 		}
@@ -111,7 +85,6 @@ void send_ping(int ping_sockfd, t_ping_info *info, t_ping_args *args) {
 		pckt.hdr.type = ICMP_ECHO;
 		pckt.hdr.un.echo.id = getpid();
 
-		// Fill data payload up to specified size (limited by packet structure)
 		size_t data_size = (args->size < sizeof(pckt.msg)) ? args->size : sizeof(pckt.msg) - 1;
 		for (i = 0; i < data_size; i++)
 			pckt.msg[i] = (char)(i + '0');
@@ -121,58 +94,41 @@ void send_ping(int ping_sockfd, t_ping_info *info, t_ping_args *args) {
 		pckt.hdr.checksum = 0;
 		pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
 
-		if (args->options & OPT_VERBOSE && msg_count == 1) {
-			printf("ft_ping: sending ICMP packets with ID=%d, data size=%zu bytes\n", getpid(), args->size);
-		}
-
 		clock_gettime(CLOCK_MONOTONIC, &time_start);
 		size_t packet_size = sizeof(t_icmphdr) + ((args->size < sizeof(pckt.msg)) ? args->size : sizeof(pckt.msg));
 		if (sendto(ping_sockfd, &pckt, packet_size, 0, (t_sockaddr *)&info->addr_con, sizeof(info->addr_con)) <= 0) {
 			printf("ft_ping: sendto: Packet sending failed\n");
-			if (args->options & OPT_VERBOSE) {
-				perror("ft_ping: sendto");
-			}
 			flag = false;
 		}
 
-		addr_len = sizeof(r_addr);
-		ssize_t bytes_received = recvfrom(ping_sockfd, rbuffer, sizeof(rbuffer), 0, (t_sockaddr *)&r_addr, &addr_len);
+		bool got_reply = false;
+		time_t start_recv_time = time(NULL);
 
-		if (bytes_received <= 0) {
-			if (msg_count > 1) {
-				if (args->options & OPT_VERBOSE) {
-					printf("ft_ping: recvfrom: timeout or error receiving packet\n");
-					perror("ft_ping: recvfrom");
-				}
-				printf("Request timeout for icmp_seq %d\n", msg_count - 1);
-			}
-		} else {
-			clock_gettime(CLOCK_MONOTONIC, &time_end);
+		while (!got_reply && (time(NULL) - start_recv_time) < tv_out.tv_sec) {
+			addr_len = sizeof(r_addr);
+			ssize_t bytes_received = recvfrom(ping_sockfd, rbuffer, sizeof(rbuffer), 0, (t_sockaddr *)&r_addr, &addr_len);
 
-			double timeElapsed = ((double)(time_end.tv_nsec - time_start.tv_nsec)) / 1000000.0;
-			rtt_msec = (time_end.tv_sec - time_start.tv_sec) * 1000.0 + timeElapsed;
-
-			if (args->options & OPT_VERBOSE) {
-				printf("ft_ping: received %zd bytes from %s\n", bytes_received, inet_ntoa(r_addr.sin_addr));
+			if (bytes_received <= 0) {
+				break;
 			}
 
-			// If packet was not sent, don't receive
-			if (flag) {
-				t_ip *ip_hdr = (t_ip *)rbuffer;
-				t_icmphdr *recv_hdr = (t_icmphdr *)(rbuffer + (ip_hdr->ip_hl * 4));
+			t_ip *ip_hdr = (t_ip *)rbuffer;
+			t_icmphdr *recv_hdr = (t_icmphdr *)(rbuffer + (ip_hdr->ip_hl * 4));
 
-				if (args->options & OPT_VERBOSE) {
-					printf("ft_ping: ICMP type=%d, code=%d, id=%d, seq=%d\n",
-						recv_hdr->type, recv_hdr->code, recv_hdr->un.echo.id, recv_hdr->un.echo.sequence);
-				}
+			if (recv_hdr->type == ICMP_ECHO) {
+				continue;
+			}
 
-				if (!(recv_hdr->type == ICMP_ECHOREPLY && recv_hdr->code == 0)) {
-					if (args->options & OPT_VERBOSE || recv_hdr->type != ICMP_ECHOREPLY) {
-						printf("ft_ping: ICMP %d/%d from %s\n", recv_hdr->type, recv_hdr->code, inet_ntoa(r_addr.sin_addr));
-					}
-				} else {
+			if (recv_hdr->type == ICMP_ECHOREPLY && recv_hdr->code == 0) {
+				if (recv_hdr->un.echo.id == getpid()) {
+					clock_gettime(CLOCK_MONOTONIC, &time_end);
+
+					double timeElapsed = ((double)(time_end.tv_nsec - time_start.tv_nsec)) / 1000000.0;
+					rtt_msec = (time_end.tv_sec - time_start.tv_sec) * 1000.0 + timeElapsed;
+
+					int icmp_payload_size = bytes_received - (ip_hdr->ip_hl * 4);
 					printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.3Lf ms\n",
-						(int)bytes_received, info->hostname, recv_hdr->un.echo.sequence, ip_hdr->ip_ttl, rtt_msec);
+						icmp_payload_size, info->hostname, recv_hdr->un.echo.sequence, ip_hdr->ip_ttl, rtt_msec);
 					msg_received_count++;
 
 					rtt_count++;
@@ -185,9 +141,15 @@ void send_ping(int ping_sockfd, t_ping_info *info, t_ping_args *args) {
 						if (rtt_msec < rtt_min) rtt_min = rtt_msec;
 						if (rtt_msec > rtt_max) rtt_max = rtt_msec;
 					}
+					got_reply = true;
 				}
 			}
 		}
+
+		if (!got_reply && flag) {
+			printf("Request timeout for icmp_seq %d\n", msg_count - 1);
+		}
+
 		if (args->count == 0 || ping_count < args->count) {
 			usleep(PING_PRECISION * args->interval);
 		}

10215941176338ba888e19725c2ce422f1e597b8
BL4 <clem29auv@gmail.com>
2025-09-12 18:49:13 +0200

    fix: correct usage output formatting in help functions for clarity

--- srcs/help.c
+++ srcs/help.c
@@ -6,7 +6,7 @@ void show_missing(char *program_name) {
 }
 
 void show_help(char *program_name) {
-	printf("Usage: %s [OPTION...] HOST\n", program_name);
+	printf("Usage: %s [OPTION...] HOST ...\n", program_name);
 	printf("Send ICMP ECHO_REQUEST packets to network hosts.\n");
 	printf("\n");
 	printf(" Options controlling ICMP request types:\n");
@@ -16,16 +16,16 @@ void show_help(char *program_name) {
 	printf("\n");
 	printf("  -c, --count=NUMBER         stop after sending NUMBER packets\n");
 	printf("  -i, --interval=NUMBER      wait NUMBER seconds between sending each packet\n");
-	printf("  -T, --tos=NUM              set type of service (TOS); to NUM\n");
+	printf("  -T, --tos=NUM              set type of service (TOS) to NUM\n");
 	printf("  -v, --verbose              verbose output\n");
 	printf("  -w, --timeout=N            stop after N seconds\n");
 	printf("  -W, --linger=N             number of seconds to wait for response\n");
 	printf("\n");
 	printf(" Options valid for --echo requests:\n");
 	printf("\n");
-	printf("  -f, --flood                flood ping (root only);\n");
+	printf("  -f, --flood                flood ping (root only)\n");
 	printf("  -l, --preload=NUMBER       send NUMBER packets as fast as possible before\n");
-	printf("                             falling into normal mode of behavior (root only);\n");
+	printf("                             falling into normal mode of behavior (root only)\n");
 	printf("  -q, --quiet                quiet output\n");
 	printf("  -s, --size=NUMBER          send NUMBER data octets\n");
 	printf("\n");
@@ -34,16 +34,15 @@ void show_help(char *program_name) {
 	printf("Mandatory or optional arguments to long options are also mandatory or optional\n");
 	printf("for any corresponding short options.\n");
 	printf("\n");
-	printf("Options marked with (root only); are available only to superuser.\n");
+	printf("Options marked with (root only) are available only to superuser.\n");
 	printf("\n");
-	printf("Report bugs to the maintaner\n");
+	printf("Report bugs to the maintainer.\n");
 }
 
 void show_usage(char *program_name) {
-	printf("Usage: %s [-vfq?] [-c NUMBER] [-i NUMBER] [-T NUM]\n", program_name);
-	printf("               [-w N] [-W N] [-l NUMBER] [-s NUMBER] [--echo]\n");
-	printf("               [--count=NUMBER] [--interval=NUMBER] [--tos=NUM]\n");
-	printf("               [--verbose] [--timeout=N] [--linger=N] [--flood]\n");
-	printf("               [--preload=NUMBER] [--quiet] [--size=NUMBER]\n");
-	printf("               [--help] [--usage] HOST ...\n");
+	printf("Usage: %s [-vfq?] [-c NUMBER] [-i NUMBER] [-T NUM] [-w N] [-W N] [-l NUMBER]\n", program_name);
+	printf("               [-s NUMBER] [--echo] [--count=NUMBER] [--interval=NUMBER]\n");
+	printf("               [--tos=NUM] [--verbose] [--timeout=N] [--linger=N] [--flood]\n");
+	printf("               [--preload=NUMBER] [--quiet] [--size=NUMBER] [--help] [--usage]\n");
+	printf("               HOST ...\n");
 }

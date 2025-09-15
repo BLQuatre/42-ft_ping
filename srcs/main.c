7f1b608b369e9cb578ece9ff372a08bfc11de13c
BL4 <clem29auv@gmail.com>
2025-09-15 14:47:35 +0200

    refactor: remove show_args function to simplify main function and improve readability

--- srcs/main.c
+++ srcs/main.c
@@ -2,24 +2,6 @@
 
 int ping_loop = 1;
 
-static void show_args(t_ping_args args) {
-	printf("adrs: [");
-	for (int i = 0; args.adresses && args.adresses[i]; i++) {
-		printf("%s%s", i > 0 ? ", " : "", args.adresses[i]);
-	}
-	printf("]");
-
-	printf(" |v: %d", args.options & OPT_VERBOSE);
-	printf(" |q: %d", args.options & OPT_QUIET);
-	printf(" |i: %ld", args.interval);
-	printf(" |c: %ld", args.count);
-	printf(" |tos: %d", args.tos);
-	printf(" |tout: %d", args.timeout);
-	printf(" |linger: %d", args.linger);
-	printf(" |s: %ld", args.size);
-	printf("\n");
-}
-
 static void intHandler(int dummy) {
 	(void) dummy;
 	ping_loop = 0;
@@ -33,7 +15,6 @@ int main(int argc, char *argv[]) {
 	argv[0] = basename(argv[0]);
 
 	args = parse_args(argc, argv);
-	show_args(args);
 
 	sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (sockfd < 0) {
